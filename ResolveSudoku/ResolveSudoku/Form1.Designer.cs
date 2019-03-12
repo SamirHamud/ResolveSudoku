4d09dfe1dac4db53292f9aaa619454246fa583e6
SAMIR ABDO HAMUD FILHO <samir_hamud@hotmail.com>
2019-03-12 14:57:23 -0300

    Resolve Sudoku em C#, concluido.

--- ResolveSudoku/ResolveSudoku/Form1.Designer.cs
+++ ResolveSudoku/ResolveSudoku/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ResolveSudoku
 {
-    partial class Form1
+    partial class FormResolve
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,13 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.BtnResolve = new System.Windows.Forms.Button();
+            this.DgvSukodu = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvSukodu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnResolve
+            // 
+            this.BtnResolve.Location = new System.Drawing.Point(273, 275);
+            this.BtnResolve.Name = "BtnResolve";
+            this.BtnResolve.Size = new System.Drawing.Size(198, 23);
+            this.BtnResolve.TabIndex = 1;
+            this.BtnResolve.Text = "Resolve Sudoku";
+            this.BtnResolve.UseVisualStyleBackColor = true;
+            this.BtnResolve.Click += new System.EventHandler(this.BtnResolve_Click);
+            // 
+            // DgvSukodu
+            // 
+            this.DgvSukodu.AllowUserToAddRows = false;
+            this.DgvSukodu.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopLeft;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Empty;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DgvSukodu.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.DgvSukodu.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DgvSukodu.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.DgvSukodu.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.DgvSukodu.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DgvSukodu.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.DgvSukodu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvSukodu.ColumnHeadersVisible = false;
+            this.DgvSukodu.EnableHeadersVisualStyles = false;
+            this.DgvSukodu.GridColor = System.Drawing.SystemColors.ActiveBorder;
+            this.DgvSukodu.Location = new System.Drawing.Point(273, 80);
+            this.DgvSukodu.Name = "DgvSukodu";
+            this.DgvSukodu.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.DgvSukodu.RowHeadersVisible = false;
+            this.DgvSukodu.RowHeadersWidth = 45;
+            this.DgvSukodu.RowTemplate.ReadOnly = true;
+            this.DgvSukodu.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.DgvSukodu.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.DgvSukodu.Size = new System.Drawing.Size(198, 189);
+            this.DgvSukodu.TabIndex = 2;
+            this.DgvSukodu.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvSukodu_CellContentClick);
+            // 
+            // FormResolve
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.DgvSukodu);
+            this.Controls.Add(this.BtnResolve);
+            this.Name = "FormResolve";
+            this.Text = "ResolveSudoku";
+            ((System.ComponentModel.ISupportInitialize)(this.DgvSukodu)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button BtnResolve;
+        private System.Windows.Forms.DataGridView DgvSukodu;
     }
 }
 
--- ResolveSudoku/ResolveSudoku/Form1.cs
+++ ResolveSudoku/ResolveSudoku/Form1.cs
@@ -5,16 +5,151 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ResolveSudoku
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
+    public partial class FormResolve : Form
+    {   // Para inserir o sudoku, só mudar os números abaixo conforme o desejado.
+        // aqui é uma variavel global para pegar os dados dela depois, que fica mais facil.
+        int[,] Sudoku = new int[9, 9] {
+        { 5, 3, 0, 0, 7, 0, 0, 0, 0 },
+        { 6, 0, 0, 1, 9, 5, 0, 0, 0 },
+        { 0, 9, 8, 0, 0, 0, 0, 6, 0 },
+        { 8, 0, 0, 0, 6, 0, 0, 0, 3 },
+        { 4, 0, 0, 8, 0, 3, 0, 0, 1 },
+        { 7, 0, 0, 0, 2, 0, 0, 0, 6 },
+        { 0, 6, 0, 0, 0, 0, 2, 8, 0 },
+        { 0, 0, 0, 4, 1, 9, 0, 0, 5 },
+        { 0, 0, 0, 0, 8, 0, 0, 7, 9 } };
+
+        public FormResolve()
+        {    //Só pra mostrar no datagrid
             InitializeComponent();
+            DgvSukodu.RowCount = 9;
+            DgvSukodu.ColumnCount = 9;
+            DgvSukodu.Columns[2].DividerWidth = 3; // para deixar igual um sudoku
+            DgvSukodu.Columns[5].DividerWidth = 3;
+            DgvSukodu.Rows[2].DividerHeight = 3;
+            DgvSukodu.Rows[5].DividerHeight = 3;
+            for (int k = 0; k < 9; k++)
+            {
+                DataGridViewColumn column = DgvSukodu.Columns[k];
+                column.AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+
+            }
+            for (int linha = 0; linha < 9; linha++)// Mostra o sudoku no datagrid.
+            {
+                for (int coluna = 0; coluna < 9; coluna++)
+                {
+                    DgvSukodu.Rows[linha].Cells[coluna].Value = Sudoku[linha, coluna];
+                }
+            }
+        }
+        private void BtnResolve_Click(object sender, EventArgs e)
+        {   // aqui é so chamar as funçôes 
+            PreencheSudoku(0, 0);
+            ApresentaGrade();          
+        }
+        int EstaDisponivel(int linha, int coluna, int digito)
+        {
+            int inicioLinha = (linha / 3) * 3,
+            inicioColuna = (coluna / 3) * 3,
+            i;
+            for (i = 0; i < 9; ++i)
+            {
+
+                if (Sudoku[linha, i] == digito) /* Verifica se o dígito já ocorre em alguma linha */
+                    return 0; /* Dígito já aparece na linha */
+
+                if (Sudoku[i, coluna] == digito)/* Verifica se o dígito já ocorre em alguma coluna */
+                    return 0; /* Dígito já aparece na coluna */
+
+                if (Sudoku[inicioLinha + (i % 3), inicioColuna + (i / 3)] == digito)/* Verifica se o dígito já ocorre em alguma subgrade */
+                    return 0; /* Dígito já aparece em subgrade */
+            }
+            /* O dígito está disponível */
+            return 1;
+        }
+        int PreencheSudoku(/*int[,] Sudoku,*/ int linha, int coluna)
+        {
+            if (linha < 9 && coluna < 9)// Ponto de parada, se ele não entrar aqui me retorna 1 e para.
+            {
+                if (Sudoku[linha, coluna] != 0)
+                {
+                    if (linha + 1 < 9)
+                        return PreencheSudoku(linha + 1, coluna);
+                    else if (coluna + 1 < 9)
+                        return PreencheSudoku(0, coluna + 1);
+                    else
+                        return 1;
+                }
+                else
+                {
+                    for (int i = 0; i < 9; i++)
+                    {
+                        if (EstaDisponivel(linha, coluna, i + 1) == 1)
+                        {   /* Se ele entrar neste if significa que o número pode ser inserido na matriz */
+                            Sudoku[linha, coluna] = i + 1;
+                            //Mostra os números sendo inseridos 
+                            Thread MostraNumeros1 = new Thread(ApresentaGrade);
+                            MostraNumeros1.Start();
+                            MostraNumeros1.Join();
+                            Thread.Sleep(70);
+                           
+                            if (linha + 1 < 9)
+                            {   /* Se a linha for de 0 a 8 ele irá tentar a resolução na próxima linha */
+                                if (PreencheSudoku(linha + 1, coluna) == 1)
+                                    return 1;
+                                else
+                                {   // Caso a resolução do if não dê certo significa que ele precisa pegar o próximo então ele irá zerar a variavel
+                                    Sudoku[linha, coluna] = 0;
+                                    //Mostra os  0 sendo inseridos.
+                                    Thread MostraNumeros2 = new Thread(ApresentaGrade);
+                                    MostraNumeros2.Start();
+                                    MostraNumeros2.Join();
+                                    Thread.Sleep(70);
+                                }
+                            }
+                            else if (coluna + 1 < 9)
+                            {
+                                if (PreencheSudoku(0, coluna + 1) == 1)
+                                    return 1;
+                                else
+                                { // Caso a resolução do if não dê certo significa que ele precisa pegar o próximo então ele irá zerar a variavel
+                                    Sudoku[linha, coluna] = 0;  // Passa linha como 0 para voltar do inicio a próxima coluna 
+                                    //Mostra os  0 sendo inseridos.
+                                    Thread MostraNumeros3 = new Thread(ApresentaGrade);
+                                    MostraNumeros3.Start();
+                                    MostraNumeros3.Join();
+                                    Thread.Sleep(70);
+                                }
+                            }
+                            else
+                                return 1;
+                        }
+                    }
+                }
+                return 0;
+            }
+            else
+                return 1;
+        }
+        void ApresentaGrade()
+        {
+            int linhas, colunas;
+            DgvSukodu.RowCount = 9;
+            DgvSukodu.ColumnCount = 9;
+            for (linhas = 0; linhas < 9; linhas++)
+            {
+                for (colunas = 0; colunas < 9; colunas++)
+                    DgvSukodu.Rows[linhas].Cells[colunas].Value = Sudoku[linhas, colunas];
+            }
+        }
+        private void DgvSukodu_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {          
         }
     }
 }
